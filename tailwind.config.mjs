2161153bc36e2bd51bf4411352fe5462ae2960eb
Jose Rojas <jgra11.2010@gmail.com>
2024-03-31 22:46:07 -0400

    container config changed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,7 @@ module.exports = {
 	prefix: '',
 	theme: {
 		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px'
-			}
+			center: true
 		},
 		extend: {
 			keyframes: {
