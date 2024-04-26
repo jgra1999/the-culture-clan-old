84d5be9775f435beba1b46db1f5995971fd56ab4
Jose Rojas <jgra11.2010@gmail.com>
2024-04-25 22:18:49 -0400

    partytown and eslint plugin added to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import netlify from "@astrojs/netlify";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  })],
+  }), partytown()],
   adapter: netlify()
 });
