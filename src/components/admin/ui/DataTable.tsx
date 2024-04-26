6c995f5c4794527a554f183e4d67991aef23dde5
Jose Rojas <jgra11.2010@gmail.com>
2024-04-25 22:26:46 -0400

    data table of the products created

--- src/components/admin/ui/DataTable.tsx
+++ src/components/admin/ui/DataTable.tsx
@@ -1,4 +1,13 @@
-export default function DataTable({ headers }: { headers: string[] }) {
+import { TrashIcon, EyeIcon, PencilIcon } from '@heroicons/react/20/solid'
+
+interface Props {
+	headers: string[]
+	data: any
+}
+export default function DataTable({ headers, data }: Props) {
+	const deleteProduct = (id: number) => {
+		console.log(id)
+	}
 	return (
 		<table className='w-full text-sm text-left text-grayText'>
 			<thead className='text-xs text-grayText uppercase bg-darkGray'>
@@ -13,7 +22,35 @@ export default function DataTable({ headers }: { headers: string[] }) {
 					</th>
 				</tr>
 			</thead>
-			<tbody>Data</tbody>
+			<tbody>
+				{data.map((item: any) => (
+					<tr key={item.id} className='border-b border-mediumGray bg-[#131313]'>
+						<th
+							scope='row'
+							className='px-6 py-4 font-medium whitespace-nowrap text-white flex gap-x-2 items-center'
+						>
+							<img src={item.image_url_1} className='w-10 h-14' alt='' />
+							<span>{item.name}</span>
+						</th>
+						<td className='px-6 py-4'>{item.collection}</td>
+						<td className='px-6 py-4'>{item.likes}</td>
+						<td className='px-6 py-4'>${item.price}</td>
+						<td className='px-6 py-4'>
+							<div className='flex gap-x-2 justify-end'>
+								<a href={`/admin/productos/ver-producto/${item.id}`}>
+									<EyeIcon className='w-5 opacity-70 hover:opacity-100' />
+								</a>
+								<a href={`/admin/productos/actualizar/${item.id}`}>
+									<PencilIcon className='w-5 opacity-70 hover:opacity-100' />
+								</a>
+								<button onClick={() => deleteProduct(item.id)}>
+									<TrashIcon className='w-5 opacity-70 hover:opacity-100' />
+								</button>
+							</div>
+						</td>
+					</tr>
+				))}
+			</tbody>
 		</table>
 	)
 }
